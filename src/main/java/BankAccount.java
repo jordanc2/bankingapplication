3fc1c3f5a100413a664e9356877d40f4d318d6ef
Charles Jordan <charlesjordan@Charless-iMac.local>
2021-10-16 17:56:43 -0500

    added menu options

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,9 +1,17 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class BankAccount {
 
     int balance;
     int previousTransaction;
     String customerName;
     String customerId;
+
+    public BankAccount(String customerName, String customerId) {
+        this.customerName = customerName;
+        this.customerId = customerId;
+    }
 
     public void deposit(int amount) {
 
@@ -31,7 +39,62 @@ public class BankAccount {
     }
 
     public void showMenu() {
-        
+        char option = '\0';
+        Scanner scanner = new Scanner(System.in, StandardCharsets.UTF_8);
+
+        System.out.println("Welcome " + customerName);
+        System.out.println("Your ID is " + customerId);
+        System.out.println("\n");
+        System.out.println("A. Check Balance");
+        System.out.println("B. Deposit");
+        System.out.println("C. Withdraw");
+        System.out.println("D. Previous Transaction");
+        System.out.println("E. Exit");
+
+        do {
+            System.out.println("==============================================================================");
+            System.out.println("Enter an option");
+            System.out.println("==============================================================================");
+
+            option = scanner.nextLine().charAt(0);
+
+            System.out.println("\n");
+
+            switch (option) {
+                case 'A':
+                    System.out.println("----------------------------------");
+                    System.out.println("Balance = " + balance);
+                    System.out.println("----------------------------------");
+                    System.out.println("\n");
+                    break;
+                case 'B':
+                    System.out.println("----------------------------------");
+                    System.out.println("Enter an amount to deposit");
+                    System.out.println("----------------------------------");
+                    int depositAmount = scanner.nextInt();
+                    deposit(depositAmount);
+                    System.out.println("\n");
+                    break;
+                case 'C':
+                    System.out.println("----------------------------------");
+                    System.out.println("Enter an amount to withdraw");
+                    System.out.println("----------------------------------");
+                    int withdrawAmount = scanner.nextInt();
+                    withdraw(withdrawAmount);
+                    System.out.println("\n");
+                case 'D':
+                    System.out.println("----------------------------------");
+                    getPreviousTransaction();
+                    System.out.println("----------------------------------");
+                    System.out.println("\n");
+                    break;
+                default:
+                    System.out.println("Invalid Option! Please try again");
+                    break;
+            }
+        } while (option != 'E');
+
+        System.out.println("Thank you for banking with Cromericore");
     }
 
 }
